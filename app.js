56680034c2eb98407428231d420ad41b2f5c4345
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-05-19 07:48:22 -0400

    Upload
    Updated uploads. Basis for creating a full project. Learned express, get request and how to pass into an html document.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { STATUS_CODES } = require('http');
 const https = require('https');
+// node module that allows the use and access of the get request from a https protocol for a website based api.
 const app = express();
 const url = "https://api.weatherapi.com/v1/current.json?key=9c6045f2530f4a24a81130713221505&q=Aiken"
 
@@ -23,11 +24,20 @@ app.get("/", function(req, res){
             const windMPH = weatherData.current.wind_mph
             const windKPH = weatherData.current.wind_kph
             const conditionText = weatherData.current.condition.text
+            const icon = weatherData.current.condition.icon
             console.log(city, region, country, tempC, tempF, currentDay, windMPH, windKPH, conditionText)
+            res.write("<p>The weather is currently " + conditionText + " in Aiken, SC.<p>");
+            // fun note... having an h2 prior to an h1 in this document will create a json packgage.
+            res.write("<img src='" + icon + "'>")
+            // when creating these inputs to pass information inside of your html files you need to write aspects of it the same way that you would if you writing a simple html document. The only difference is, is the quotations and the addition of your variables.
+            res.write("<h1>The temperature in Aiken is " + tempF + " degress farenheit.</h1>")
+            res.send();
+            // only one app res.send method can be used in any given document. choose wisely. You can res.write as many beforehand. res.send is the last action that can be done however. res.send can not be a written item when there is already a res.write occuring.
         })
     })
 
-    res.send("Server is up and running.")
+    // res.send("Server is up and running.")
+    // this was the original res.send request for this file. Has to be removed in order for app to work correct. Only one res.send request can be used in any given document.
 })
 
 app.listen(3000, function() {
